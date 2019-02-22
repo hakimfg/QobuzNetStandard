887c1175763f984c956b2e3dbe1e4caa014928e4
Hakimuddin Gadawala <hakimfg@gmail.com>
2019-02-22 16:30:04 +0530

    Unit Test Improvement

--- UnitTestDLL/UnitTest1.cs
+++ UnitTestDLL/UnitTest1.cs
@@ -7,167 +7,238 @@ namespace UnitTestDLL
     [TestClass]
     public class UnitTest1
     {
+        string _testUsername = "test@test.com";
+        string _testPassword = "testpassword";
+
         [TestMethod]
         public void Login()
         {
             Service service = new Service();
+            User user;
             try
             {
-                User user = service.Login("saif@gmail.com", "testpwd");
-
-                Assert.IsTrue(user != null && !string.IsNullOrEmpty(user.UserAuthToken));
+                user = service.Login(_testUsername, _testPassword);
             }
-            catch (Exception)
+            catch
             {
-                Assert.Fail();
+                throw;
             }
+            Assert.IsTrue(user != null && !string.IsNullOrEmpty(user.UserAuthToken));
         }
 
         [TestMethod]
         public void SearchTrack()
         {
             Service service = new Service();
+            Tracks tracks;
             try
             {
-                User user = service.Login("saif@gmail.com", "testpwd");
-
-                Assert.IsTrue(user != null && !string.IsNullOrEmpty(user.UserAuthToken));
-
-                Tracks tracks = service.SearchTrack("Hello", 0, 0);
-
-                if (tracks == null)
-                {
-                    Assert.Fail();
-                }
-                else
-                {
-                    Assert.IsTrue(tracks.Total >= 0);
-                }
+                tracks = service.SearchTrack("Hello", 0, 0);
             }
-            catch (Exception)
+            catch
             {
-                Assert.Fail();
+                throw;
             }
+            Assert.IsNotNull(tracks);
         }
 
         [TestMethod]
         public void CreatePlaylist()
         {
             Service service = new Service();
+
+            User user;
             try
             {
-                User user = service.Login("saif@gmail.com", "testpwd");
-
-                Assert.IsTrue(user != null && !string.IsNullOrEmpty(user.UserAuthToken));
-
-                Playlist playlist = service.CreatePlaylist("Sample Test");
-
-                Assert.IsTrue(playlist != null);
+                user = service.Login(_testUsername, _testPassword);
             }
-            catch (Exception)
+            catch
             {
-                Assert.Fail();
+                throw;
             }
+            Assert.IsTrue(user != null && !string.IsNullOrEmpty(user.UserAuthToken));
+
+            Playlist playlist;
+            try
+            {
+                playlist = service.CreatePlaylist("Sample Test");
+            }
+            catch
+            {
+                throw;
+            }
+
+            Assert.IsNotNull(playlist);
         }
 
         [TestMethod]
         public void CreatePlaylistWithItems()
         {
             Service service = new Service();
+
+            User user;
             try
             {
-                User user = service.Login("saif@gmail.com", "testpwd");
-
-                Assert.IsTrue(user != null && !string.IsNullOrEmpty(user.UserAuthToken));
-               
-                Tracks tracks = service.SearchTrack("Hello", 0, 0);
-
-                Assert.IsTrue(tracks != null);
-
-                Playlist playlist = service.CreatePlaylistWithItems("Sample Playlist", tracks.Items[0].Id.ToString());
-
-                Assert.IsTrue(playlist != null);
+                user = service.Login(_testUsername, _testPassword);
             }
-            catch (Exception)
+            catch
             {
-                Assert.Fail();
+                throw;
             }
+            Assert.IsTrue(user != null && !string.IsNullOrEmpty(user.UserAuthToken));
+
+            Tracks tracks;
+            try
+            {
+                tracks = service.SearchTrack("Hello", 0, 0);
+            }
+            catch
+            {
+                throw;
+            }
+            Assert.IsNotNull(tracks);
+
+            Playlist playlist;
+            try
+            {
+                playlist = service.CreatePlaylistWithItems("Sample Playlist", tracks.Items[0].Id.ToString());
+            }
+            catch
+            {
+                throw;
+            }
+            Assert.IsNotNull(playlist);
         }
 
         [TestMethod]
         public void GetUserPlaylist()
         {
             Service service = new Service();
+
+            User user;
             try
             {
-                User user = service.Login("saif@gmail.com", "testpwd");
-
-                Assert.IsTrue(user != null && !string.IsNullOrEmpty(user.UserAuthToken));
-
-                Playlists playlists = service.GetUserPlaylist(0,0);
-
-                Assert.IsTrue(playlists != null);
+                user = service.Login(_testUsername, _testPassword);
             }
-            catch (Exception)
+            catch
             {
-                Assert.Fail();
+                throw;
             }
+            Assert.IsTrue(user != null && !string.IsNullOrEmpty(user.UserAuthToken));
+
+            Playlists playlists;
+            try
+            {
+                playlists = service.GetUserPlaylist(0, 0);
+            }
+            catch
+            {
+                throw;
+            }
+            Assert.IsNotNull(playlists);
+
         }
 
         [TestMethod]
         public void AddItemToPlaylist()
         {
             Service service = new Service();
+
+            User user;
             try
             {
-                User user = service.Login("saif@gmail.com", "testpwd");
-
-                Assert.IsTrue(user != null && !string.IsNullOrEmpty(user.UserAuthToken));
-
-                Playlists playlists = service.GetUserPlaylist(0, 0);
-
-                Assert.IsTrue(playlists != null);
-
-                Tracks tracks = service.SearchTrack("Hello", 0, 0);
-
-                Assert.IsTrue(tracks != null);
-
-                Playlist playlist = service.AddItemToPlaylist(playlists.Items[0].Id, tracks.Items[0].Id.ToString());
-
-                Assert.IsTrue(playlist != null);
+                user = service.Login(_testUsername, _testPassword);
             }
-            catch (Exception)
+            catch
             {
-                Assert.Fail();
+                throw;
             }
+            Assert.IsTrue(user != null && !string.IsNullOrEmpty(user.UserAuthToken));
+
+            Playlists playlists;
+            try
+            {
+                playlists = service.GetUserPlaylist(0, 0);
+            }
+            catch
+            {
+                throw;
+            }
+            Assert.IsTrue(playlists != null);
+
+            Tracks tracks;
+            try
+            {
+                tracks = service.SearchTrack("Hello", 0, 0);
+            }
+            catch
+            {
+                throw;
+            }
+            Assert.IsTrue(tracks != null);
+
+            Playlist playlist;
+            try
+            {
+                playlist = service.AddItemToPlaylist(playlists.Items[0].Id, tracks.Items[0].Id.ToString());
+            }
+            catch
+            {
+                throw;
+            }
+            Assert.IsTrue(playlist != null);
+
         }
 
         [TestMethod]
         public void RemoveItemFromPlaylist()
         {
             Service service = new Service();
+
+            User user;
             try
             {
-                User user = service.Login("saif@gmail.com", "testpwd");
-
-                Assert.IsTrue(user != null && !string.IsNullOrEmpty(user.UserAuthToken));
-
-                Playlists playlists = service.GetUserPlaylist(0, 0);
-
-                Assert.IsTrue(playlists != null);
-
-                Tracks tracks = service.SearchTrack("Hello", 0, 0);
-
-                Assert.IsTrue(tracks != null);
-
-                bool res = service.RemoveItemFromPlaylist(playlists.Items[0].Id, tracks.Items[0].Id.ToString());
-
-                Assert.IsTrue(res);
+                user = service.Login(_testUsername, _testPassword);
             }
-            catch (Exception)
+            catch
             {
-                Assert.Fail();
+                throw;
             }
+            Assert.IsTrue(user != null && !string.IsNullOrEmpty(user.UserAuthToken));
+
+            Playlists playlists;
+            try
+            {
+                playlists = service.GetUserPlaylist(0, 0);
+            }
+            catch
+            {
+                throw;
+            }
+            Assert.IsTrue(playlists != null);
+
+            Tracks tracks;
+            try
+            {
+                tracks = service.SearchTrack("Hello", 0, 0);
+            }
+            catch
+            {
+                throw;
+            }
+            Assert.IsTrue(tracks != null);
+
+            bool res;
+            try
+            {
+                res = service.RemoveItemFromPlaylist(playlists.Items[0].Id, tracks.Items[0].Id.ToString());
+            }
+            catch
+            {
+                throw;
+            }
+            Assert.IsTrue(res);
+              
         }
     }
 }
